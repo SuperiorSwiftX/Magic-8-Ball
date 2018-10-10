a880dd0bcc26970e9872548bf0b5807e313d0609
SuperMacUser <eriksson.hans@gmail.com>
2018-10-10 18:28:06 +0200

    Magic 8 Ball

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Hans Eriksson on 2018-10-10.
+//  Copyright © 2018 SuperiorSwiftX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
